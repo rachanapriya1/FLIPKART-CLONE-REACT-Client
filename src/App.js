30bfd87dc8baf984dbc6aaccf9f197bb16db439c
Rachana Martha <martharachanapriya@gmail.com>
2024-05-05 13:04:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, NotFound } from './Components/default';
+import { Box } from '@mui/material'
+
+//components
+import Header from './Components/Header/Header';
+import DetailView from './Components/ItemDetails/DetailView';
+import TemplateProvider from './templates/TemplateProvider';
+import ContextProvider from './context/ContextProvider';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TemplateProvider>
+      <ContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Box style={{marginTop: 54}}>
+            <Routes>
+              <Route path= '/' element={<Home />} />
+              <Route path= '/cart' element={<Cart />} />
+              <Route path= '/product/:id' element={<DetailView />} />
+            </Routes>
+          </Box>
+        </BrowserRouter>
+      </ContextProvider>
+    </TemplateProvider>
   );
 }
 
